47c7d9b6b9960849b2726140e018de4735d1db65
RadRasyad <rafidanyr21@gmail.com>
2022-11-15 14:21:12 +0700

    detail img view

--- app/src/main/java/com/hacktiv8/bux/ui/bus/BusDetailActivity.java
+++ app/src/main/java/com/hacktiv8/bux/ui/bus/BusDetailActivity.java
@@ -3,7 +3,12 @@ package com.hacktiv8.bux.ui.bus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.hacktiv8.bux.R;
 import com.hacktiv8.bux.databinding.ActivityBusDetailBinding;
 
 public class BusDetailActivity extends AppCompatActivity {
@@ -21,5 +26,20 @@ public class BusDetailActivity extends AppCompatActivity {
         });
 
 
+        binding.btnSeePicture.setOnClickListener(v -> {
+            onImageShow("pathImg");
+        });
+
+    }
+
+    private void onImageShow(String uri) {
+        View inflatedView = getLayoutInflater().inflate(R.layout.img_view_layout, null);
+        ImageView imageView = inflatedView.findViewById(R.id.ivViewer);
+        Glide.with(this)
+                .load(uri)
+                .placeholder(R.drawable.bux_logo)
+                .into(imageView);
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setView(inflatedView).show();
     }
 }
