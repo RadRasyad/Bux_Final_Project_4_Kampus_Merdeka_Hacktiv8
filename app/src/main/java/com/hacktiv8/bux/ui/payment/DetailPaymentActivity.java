6eee2092a2d4cd25c0b8b12f447927b754d90687
Syaifun Nadhif <88659346+SyaifunNadhif@users.noreply.github.com>
2022-11-16 22:00:22 +0700

    layout rating, payment

---
+++ app/src/main/java/com/hacktiv8/bux/ui/payment/DetailPaymentActivity.java
@@ -0,0 +1,16 @@
+package com.hacktiv8.bux.ui.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hacktiv8.bux.R;
+
+public class DetailPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_payment);
+    }
+}
