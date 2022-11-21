1d4a206a17855f9a72a4c224b98e84d875173626
FathimahKhonsa <khonsa.fathimah@gmail.com>
2022-11-21 20:50:15 +0700

    credit card verification layout

---
+++ app/src/main/java/com/hacktiv8/bux/ui/payment/CreditCardVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.hacktiv8.bux.ui.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hacktiv8.bux.R;
+
+public class CreditCardVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_verification);
+    }
+}
