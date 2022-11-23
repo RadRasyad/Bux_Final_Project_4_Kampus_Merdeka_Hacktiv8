9428ed7a8edee1b3ed797a68acc9d221786415b4
FathimahKhonsa <khonsa.fathimah@gmail.com>
2022-11-23 21:26:46 +0700

    retail payment verification layout

---
+++ app/src/main/java/com/hacktiv8/bux/ui/payment/RetailPaymentVerificationActivity.java
@@ -0,0 +1,16 @@
+package com.hacktiv8.bux.ui.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hacktiv8.bux.R;
+
+public class RetailPaymentVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retail_payment_verification);
+    }
+}
